fb593c8dc0878eb9c04e0b8516283612c8ec2e79
AbdallahMohamed79 <113598906+AbdallahMohamed79@users.noreply.github.com>
2024-03-12 16:31:07 +0200

    Update usersController.php

--- app/Http/Controllers/api/usersController.php
+++ app/Http/Controllers/api/usersController.php
@@ -30,10 +30,8 @@ class usersController extends Controller
         //
         $validator = Validator::make($request->all(), [
             "name"=>"required|min:3",
-            // "lname"=>"required|min:3",
             "email" => "required|email|unique:users,email",
             "phone"=>"required|min:11",
-            // "image" => "required|image|mimes:jpg,jpeg,png,bmp,gif,svg,webp",
             "password"=>"required|min:8"
         ]);
         if($validator->fails()){
@@ -62,9 +60,7 @@ class usersController extends Controller
           $validator = Validator::make($request->all(), [
             "email"=> [Rule::unique('users')->ignore($user->id)],
              "name"=>"required|min:3",
-            // "lname"=>"required|min:3",
             "phone"=>"required|min:11",
-             "image" => "required|image|mimes:jpg,jpeg,png,bmp,gif,svg,webp",
             "password"=>"required|min:8"
         ]);
         if($validator->fails()){
