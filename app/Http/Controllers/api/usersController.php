c3618e429b1ab6b5215b457eb25b8997ff4bd2f7
mohamed <mohamedmahmoud1999@gmail.com>
2024-03-12 20:12:08 +0200

    apitrait

--- app/Http/Controllers/api/usersController.php
+++ app/Http/Controllers/api/usersController.php
@@ -7,13 +7,20 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Validation\Rule;
 use Illuminate\Support\Facades\Validator;
-
+use Illuminate\Support\Facades\Auth;
 class usersController extends Controller
 {
+// trait api 
+
+use traitapi\apitrait;
+
+
     public function index()
     {
-    
-         return User::all();        
+        
+        $user = User::all();
+   
+         return $this->apiresponse($user,"ok",200);      
 
 
         }
@@ -34,7 +41,7 @@ class usersController extends Controller
             return response($validator->errors()->all());
         }
         $user = User::create($request->all());
-        return response($user, 201);
+        return $this->apiresponse($user,"ok",201);  
       
     }
 
@@ -42,7 +49,7 @@ class usersController extends Controller
     public function show(User $user)
     {
        
-        return $user;
+        return  $this->apiresponse($user,"ok",200);  
     }
 
 
@@ -52,15 +59,16 @@ class usersController extends Controller
           $validator = Validator::make($request->all(), [
             "email"=> [Rule::unique('users')->ignore($user->id)],
              "name"=>"required|min:3",
-            "phone"=>"required|min:11",
-            "image" => 'required','max:1000','mimes:png,jpg,jpeg',
+             'phone' => ['required', 'regex:/^01[0-2]{1}[0-9]{8}$/'],
+             "image" => 'required','max:1000','mimes:png,jpg,jpeg',
             "password"=>"required|min:8"
         ]);
         if($validator->fails()){
             return response()->json(['message' => "Errors", 'data' => $validator->errors()->all()], 422);
         }
         $user->update($request->all());
-        return response()->json(['message' => "User updated succcfully", 'data' => $user], 201);
+    
+        return   $this->apiresponse($user,"User updated succcfully",201); 
 
     }
 
@@ -69,6 +77,6 @@ class usersController extends Controller
     {
      
           $user->delete();
-        return response("delete", 204);
+        return $this->apiresponse($user,"User delete succcfully",201); 
     }
 }
