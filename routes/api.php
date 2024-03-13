141b02769b19246e41329423cf8d3cad6b910f3d
zainab <mahmoudzainab040@gmail.com>
2024-03-13 02:04:56 +0200

    add api review

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\api\ReviewController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\api\usersController;
@@ -15,3 +16,19 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 // Route::post('/login', [usersController::class, 'login']);
 Route::apiResource("users",usersController::class);
+
+
+//// Route For review /////////
+
+Route::apiResource("reviews",ReviewController::class);
+
+/*
+
+ GET|HEAD        api/reviews ....... reviews.index › api\ReviewController@index  
+  POST            api/reviews ... reviews.store › api\ReviewController@store  
+  GET|HEAD        api/reviews/{review} ...... reviews.show › api\ReviewController@show  
+  PUT|PATCH       api/reviews/{review} ..... reviews.update › api\ReviewController@update  
+  DELETE          api/reviews/{review}.... reviews.destroy › api\ReviewController@destroy  
+
+ 
+  */
