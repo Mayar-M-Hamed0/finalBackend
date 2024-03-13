1557a2689bc53c6965efb3b70e318797e519238b
Anan Mohsen Mahmoud <ananelemery@gmail.com>
2024-03-13 13:44:14 +0200

    Committing changes to routes/api.php

--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\api\usersController;
-
-
+use App\Http\Controllers\ServiceCenterController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
@@ -15,3 +14,9 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 
 Route::apiResource("users",usersController::class);
+
+Route::get('/service-centers', [ServiceCenterController::class, 'index']);
+Route::post('/service-centers', [ServiceCenterController::class, 'store']);
+Route::get('/service-centers/{serviceCenter}', [ServiceCenterController::class, 'show']);
+Route::put('/service-centers/{serviceCenter}', [ServiceCenterController::class, 'update']);
+Route::delete('/service-centers/{serviceCenter}', [ServiceCenterController::class, 'destroy']);
