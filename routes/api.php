9638cb405d0f82aaf7eb1692a19514458ec4a396
Mayar-M-Hamed0 <mayar.m.hamed97@gmail.com>
2024-03-13 02:47:15 +0200

    orders api

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\api\ordersController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\api\usersController;
@@ -15,3 +16,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 // Route::post('/login', [usersController::class, 'login']);
 Route::apiResource("users",usersController::class);
+Route::apiResource("orders",ordersController::class);
